d0392292c7f94e9028ba1e92cb94525b4160b394
Ozan Tellioglu <ozntel@gmail.com>
2021-06-10 12:18:30 +0200

    Recursive Folder Tree Creation

--- src/components/FolderComponent.tsx
+++ src/components/FolderComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { App, TFolder } from 'obsidian';
 import Tree from './treeComponent/TreeComponent';
 
@@ -13,6 +13,27 @@ export function FolderComponent({ app, activeFolderPath, setActiveFolderPath, se
 
     const rootFolder: TFolder = app.vault.getRoot()
     const treeStyles = { color: 'white', fill: 'white', width: '100%', left: 10, top: 10 }
+    const [folderTree, setFolderTree] = useState(null);
+
+    useEffect(() => {
+        setFolderTree(createFolderTree(rootFolder));
+    }, [])
+
+    const createFolderTree = (startFolder: TFolder) => {
+        const fileTree: { folder: TFolder, children: any } = { folder: startFolder, children: [] }
+        function recursive(folder: TFolder, object: { folder: TFolder, children: any }) {
+            for (let child of folder.children) {
+                if (child instanceof TFolder) {
+                    let childFolder: TFolder = (child as TFolder);
+                    let newObj: { folder: TFolder, children: any } = { folder: childFolder, children: [] }
+                    object.children.push(newObj);
+                    if (childFolder.children) recursive(childFolder, newObj);
+                }
+            }
+        }
+        recursive(startFolder, fileTree);
+        return fileTree;
+    }
 
     return (
         <React.Fragment>
