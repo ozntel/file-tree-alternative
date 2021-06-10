f68f64bc2b0121866d4f1a4bf4200683faa88119
Ozan Tellioglu <ozntel@gmail.com>
2021-06-10 12:56:55 +0200

    Changes

--- src/components/FolderComponent.tsx
+++ src/components/FolderComponent.tsx
@@ -9,11 +9,16 @@ interface FolderProps {
     setView: Function
 }
 
+interface FolderTree {
+    folder: TFolder,
+    children: FolderTree[]
+}
+
 export function FolderComponent({ app, activeFolderPath, setActiveFolderPath, setView }: FolderProps) {
 
     const rootFolder: TFolder = app.vault.getRoot()
     const treeStyles = { color: 'white', fill: 'white', width: '100%', left: 10, top: 10 }
-    const [folderTree, setFolderTree] = useState(null);
+    const [folderTree, setFolderTree] = useState<FolderTree>(null);
 
     useEffect(() => {
         setFolderTree(createFolderTree(rootFolder));
@@ -38,12 +43,15 @@ export function FolderComponent({ app, activeFolderPath, setActiveFolderPath, se
     return (
         <React.Fragment>
             <Tree content={app.vault.getName()} open style={treeStyles}>
-                <NestedChildrenComponent
-                    folder={rootFolder}
-                    activeFolderPath={activeFolderPath}
-                    setActiveFolderPath={setActiveFolderPath}
-                    setView={setView}
-                />
+                {
+                    folderTree &&
+                    <NestedChildrenComponent
+                        folderTree={folderTree}
+                        activeFolderPath={activeFolderPath}
+                        setActiveFolderPath={setActiveFolderPath}
+                        setView={setView}
+                    />
+                }
             </Tree>
         </React.Fragment>
     )
@@ -52,14 +60,14 @@ export function FolderComponent({ app, activeFolderPath, setActiveFolderPath, se
 /* ------ Nested Children Component ------ */
 
 interface NestedChildrenComponentProps {
-    folder: TFolder,
+    folderTree: FolderTree,
     activeFolderPath: string,
     setActiveFolderPath: Function,
     setView: Function
 }
 
-function NestedChildrenComponent({ folder, activeFolderPath, setActiveFolderPath, setView }: NestedChildrenComponentProps) {
-    if (!folder.children) return null;
+function NestedChildrenComponent({ folderTree, activeFolderPath, setActiveFolderPath, setView }: NestedChildrenComponentProps) {
+    if (!folderTree.children) return null;
 
     const handleFolderNameClick = (folderPath: string) => {
         setActiveFolderPath(folderPath);
@@ -74,27 +82,26 @@ function NestedChildrenComponent({ folder, activeFolderPath, setActiveFolderPath
     return (
         <React.Fragment>
             {
-                Array.isArray(folder.children) &&
-                folder.children.filter(child => child instanceof TFolder)
-                    .map(child => {
-                        return (
-                            <React.Fragment key={child.path}>
-                                {
-                                    (child as TFolder).children.some(child => child instanceof TFolder) ?
-                                        <Tree content={child.name} open={isTreeOpen(child.name) ? true : false} onClick={() => handleFolderNameClick(child.path)}>
-                                            <NestedChildrenComponent
-                                                folder={(child as TFolder)}
-                                                activeFolderPath={activeFolderPath}
-                                                setActiveFolderPath={setActiveFolderPath}
-                                                setView={setView}
-                                            />
-                                        </Tree>
-                                        :
-                                        <Tree content={child.name} onClick={() => handleFolderNameClick(child.path)} />
-                                }
-                            </React.Fragment>
-                        )
-                    })
+                Array.isArray(folderTree.children) &&
+                folderTree.children.map(child => {
+                    return (
+                        <React.Fragment key={child.folder.path}>
+                            {
+                                (child.folder as TFolder).children.some(child => child instanceof TFolder) ?
+                                    <Tree content={child.folder.name} open={isTreeOpen(child.folder.name) ? true : false} onClick={() => handleFolderNameClick(child.folder.path)}>
+                                        <NestedChildrenComponent
+                                            folderTree={child}
+                                            activeFolderPath={activeFolderPath}
+                                            setActiveFolderPath={setActiveFolderPath}
+                                            setView={setView}
+                                        />
+                                    </Tree>
+                                    :
+                                    <Tree content={child.folder.name} onClick={() => handleFolderNameClick(child.folder.path)} />
+                            }
+                        </React.Fragment>
+                    )
+                })
             }
         </React.Fragment>
     )
