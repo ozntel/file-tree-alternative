a4ee726479bdb5c557786412362a2b29be255daf
Ozan Tellioglu <ozntel@gmail.com>
2021-09-09 01:04:23 +0200

    Main Component Conversion

--- src/components/MainView/MainComponent.tsx
+++ src/components/MainView/MainComponent.tsx
@@ -1,217 +1,170 @@
-import { App, TAbstractFile, TFile, TFolder } from 'obsidian';
-import React from 'react';
+import { TAbstractFile, TFile, TFolder } from 'obsidian';
+import React, { useState, useEffect } from 'react';
 import { FileComponent } from 'components/FileView/FileComponent';
 import { FolderComponent } from 'components/FolderView/FolderComponent';
 import { FileTreeView } from 'FileTreeView';
 import FileTreeAlternativePlugin from 'main';
 import * as FileTreeUtils from 'utils/Utils';
-import { FolderTree } from 'utils/types';
+import { FolderTree, FolderFileCountMap } from 'utils/types';
 
 interface MainTreeComponentProps {
 	fileTreeView: FileTreeView;
 	plugin: FileTreeAlternativePlugin;
 }
 
-interface MainTreeComponentState {
-	view: string;
-	activeFolderPath: string;
-	fileList: TFile[];
-	pinnedFiles: TFile[];
-	openFolders: TFolder[];
-	folderTree: FolderTree;
-	excludedExtensions: string[];
-	excludedFolders: string[];
-	folderFileCountMap: { [key: string]: number };
-}
+export default function MainTreeComponent(props: MainTreeComponentProps) {
+	// --> Main Variables
+	const plugin: FileTreeAlternativePlugin = props.plugin;
+	const rootFolder: TFolder = plugin.app.vault.getRoot();
+	const excludedExtensions: string[] = getExcludedExtensions();
 
-export default class MainTreeComponent extends React.Component<MainTreeComponentProps, MainTreeComponentState> {
-	state = {
-		view: 'folder',
-		activeFolderPath: '',
-		fileList: [] as TFile[],
-		pinnedFiles: [] as TFile[],
-		openFolders: [] as TFolder[],
-		folderTree: null as FolderTree,
-		excludedExtensions: [] as string[],
-		excludedFolders: [] as string[],
-		folderFileCountMap: {},
+	// --> Register Event Handlers
+	plugin.registerEvent(plugin.app.vault.on('rename', (file) => handleVaultChanges(file, 'rename')));
+	plugin.registerEvent(plugin.app.vault.on('delete', (file) => handleVaultChanges(file, 'delete')));
+	plugin.registerEvent(plugin.app.vault.on('create', (file) => handleVaultChanges(file, 'create')));
+
+	// --> Plugin States
+	const [view, setView] = useState<string>('folder');
+	const [activeFolderPath, setActiveFolderPath] = useState<string>('');
+	const [fileList, setFileList] = useState<TFile[]>([]);
+	const [pinnedFiles, setPinnedFiles] = useState<TFile[]>(getPinnedFilesFromSettings());
+	const [openFolders, setOpenFolders] = useState<TFolder[]>(getOpenFoldersFromSettings());
+	const [folderTree, setFolderTree] = useState<FolderTree>(FileTreeUtils.createFolderTree(rootFolder));
+	const [excludedFolders, setExcludedFolders] = useState<string[]>(getExcludedFolders());
+	const [folderFileCountMap, setFolderFileCountMap] = useState<FolderFileCountMap>(
+		plugin.settings.folderCount ? FileTreeUtils.getFolderNoteCountMap(plugin) : {}
+	);
+
+	const setNewFileList = (folderPath?: string) => {
+		let filesPath = folderPath ? folderPath : activeFolderPath;
+		setFileList(FileTreeUtils.getFilesUnderPath(filesPath, plugin));
 	};
 
-	plugin = this.props.plugin;
-	rootFolder: TFolder = this.plugin.app.vault.getRoot();
+	// State Change Handlers
+	useEffect(() => savePinnedFilesToSettings(), [pinnedFiles]);
+	useEffect(() => saveOpenFoldersToSettings(), [openFolders]);
+	useEffect(() => saveExcludedFoldersToSettings(), [excludedFolders]);
 
-	setView = (view: string) => this.setState({ view });
-
-	setPinnedFiles = (pinnedFiles: TFile[]) => {
-		this.setState({ pinnedFiles }, () => this.savePinnedFilesToSettings());
-	};
-
-	setFileList = (fileList: TFile[]) => {
-		this.setState({ fileList });
-	};
-
-	setNewFileList = (folderPath?: string) => {
-		let filesPath = folderPath ? folderPath : this.state.activeFolderPath;
-		this.setState({ fileList: FileTreeUtils.getFilesUnderPath(filesPath, this.plugin) });
-	};
-
-	// Folder Component to Set Expanded Folders
-	setOpenFolders = (openFolders: TFolder[]) => {
-		this.setState({ openFolders }, () => this.saveOpenFoldersToSettings());
-	};
-
-	// Function used for File View
-	setActiveFolderPath = (activeFolderPath: string) => {
-		// If activeFolderPath is set, it means it should go to 'file' view
-		this.setState({ activeFolderPath: activeFolderPath });
-		this.setNewFileList(activeFolderPath);
-		this.setState({ view: 'file' });
-	};
+	// If activeFolderPath is set, it means it should go to 'file' view
+	useEffect(() => {
+		if (activeFolderPath !== '') {
+			setNewFileList(activeFolderPath);
+			setView('file');
+		}
+	}, [activeFolderPath]);
 
 	// Load Excluded Extensions as State
-	loadExcludedExtensions = () => {
-		let extensionsString: string = this.plugin.settings.excludedExtensions;
+	function getExcludedExtensions(): string[] {
+		let extensionsString: string = plugin.settings.excludedExtensions;
 		let excludedExtensions: string[] = [];
 		for (let extension of extensionsString.split(',')) {
 			excludedExtensions.push(extension.trim());
 		}
-		this.setState({ excludedExtensions });
-	};
+		return excludedExtensions;
+	}
 
 	// Load Excluded Folders
-	loadExcludedFolders = () => {
-		let excludedString: string = this.plugin.settings.excludedFolders;
+	function getExcludedFolders(): string[] {
+		let excludedString: string = plugin.settings.excludedFolders;
 		let excludedFolders: string[] = [];
 		for (let excludedFolder of excludedString.split(',')) {
 			excludedFolders.push(excludedFolder.trim());
 		}
-		this.setState({ excludedFolders });
-	};
+		return excludedFolders;
+	}
 
 	// Load The String List and Set Open Folders State
-	loadOpenFoldersFromSettings() {
+	function getOpenFoldersFromSettings(): TFolder[] {
 		let openFolders: TFolder[] = [];
-		for (let folder of this.plugin.settings.openFolders) {
-			let openFolder = this.plugin.app.vault.getAbstractFileByPath(folder);
+		for (let folder of plugin.settings.openFolders) {
+			let openFolder = plugin.app.vault.getAbstractFileByPath(folder);
 			if (openFolder) openFolders.push(openFolder as TFolder);
 		}
-		this.setState({ openFolders });
+		return openFolders;
 	}
 
 	// Load The String List anad Set Pinned Files State
-	loadPinnedFilesFromSettings() {
+	function getPinnedFilesFromSettings(): TFile[] {
 		let pinnedFiles: TFile[] = [];
-		for (let file of this.plugin.settings.pinnedFiles) {
-			let pinnedFile = this.plugin.app.vault.getAbstractFileByPath(file);
+		for (let file of plugin.settings.pinnedFiles) {
+			let pinnedFile = plugin.app.vault.getAbstractFileByPath(file);
 			if (pinnedFile) pinnedFiles.push(pinnedFile as TFile);
 		}
-		this.setState({ pinnedFiles });
+		return pinnedFiles;
 	}
 
 	// Get The Folders State and Save in Data as String Array
-	saveOpenFoldersToSettings() {
-		let openFolders: string[] = [];
-		for (let folder of this.state.openFolders) {
-			openFolders.push(folder.path);
+	function saveOpenFoldersToSettings() {
+		let openFoldersToSave: string[] = [];
+		for (let folder of openFolders) {
+			openFoldersToSave.push(folder.path);
 		}
-		this.plugin.settings.openFolders = openFolders;
-		this.plugin.saveSettings();
+		plugin.settings.openFolders = openFoldersToSave;
+		plugin.saveSettings();
 	}
 
 	// Get The Pinned Files State and Save in Data as String Array
-	savePinnedFilesToSettings() {
-		let pinnedFiles: string[] = [];
-		for (let file of this.state.pinnedFiles) {
-			pinnedFiles.push(file.path);
+	function savePinnedFilesToSettings() {
+		let pinnedFilesToSave: string[] = [];
+		for (let file of pinnedFiles) {
+			pinnedFilesToSave.push(file.path);
 		}
-		this.plugin.settings.pinnedFiles = pinnedFiles;
-		this.plugin.saveSettings();
+		plugin.settings.pinnedFiles = pinnedFilesToSave;
+		plugin.saveSettings();
 	}
 
 	// Save Excluded Folders to Settings as String
-	saveExcludedFoldersToSettings() {
-		this.plugin.settings.excludedFolders = this.state.excludedFolders.join(', ');
-		this.plugin.saveSettings();
-	}
-
-	// Set New Excluded Folders List and Save to Settings
-	setExcludedFolders = (excludedFolders: string[]) => {
-		this.setState({ excludedFolders });
-		this.saveExcludedFoldersToSettings();
-	};
-
-	// First Time Compount Mount
-	componentDidMount() {
-		console.log('File Tree Component Mounted');
-		// Set the Folder Tree and Folder Count Map
-		this.setState({ folderTree: FileTreeUtils.createFolderTree(this.rootFolder) });
-		if (this.plugin.settings.folderCount) this.setState({ folderFileCountMap: FileTreeUtils.getFolderNoteCountMap(this.plugin) });
-		// Set/Remember Open Folders from Last Session
-		this.loadOpenFoldersFromSettings();
-		// Set/Remember Pinned Files
-		this.loadPinnedFilesFromSettings();
-		// Set Excluded File Extensions
-		this.loadExcludedExtensions();
-		// Set Excluded Folders
-		this.loadExcludedFolders();
-		// Register Vault Events
-		this.plugin.registerEvent(this.plugin.app.vault.on('rename', (file) => this.handleVaultChanges(file, 'rename')));
-		this.plugin.registerEvent(this.plugin.app.vault.on('delete', (file) => this.handleVaultChanges(file, 'delete')));
-		this.plugin.registerEvent(this.plugin.app.vault.on('create', (file) => this.handleVaultChanges(file, 'create')));
+	function saveExcludedFoldersToSettings() {
+		plugin.settings.excludedFolders = excludedFolders.join(', ');
+		plugin.saveSettings();
 	}
 
 	// Function for Event Handlers
-	handleVaultChanges = (file: TAbstractFile, changeType: string) => {
+	function handleVaultChanges(file: TAbstractFile, changeType: string) {
 		if (file instanceof TFile) {
-			if (this.state.view === 'file') {
+			if (view === 'file') {
 				if (changeType === 'rename' || changeType === 'delete') {
 					// If the file renamed and deleted is in the current view, it will be updated
-					if (this.state.fileList.some((stateFile) => stateFile.path === file.path)) {
-						this.setNewFileList();
-					}
+					if (fileList.some((stateFile) => stateFile.path === file.path)) setNewFileList();
 				} else if (changeType === 'create') {
-					if (file.path.match(new RegExp(this.state.activeFolderPath + '.*'))) {
-						this.setNewFileList();
-					}
+					if (file.path.match(new RegExp(activeFolderPath + '.*'))) setNewFileList();
 				}
 			}
 		} else if (file instanceof TFolder) {
-			this.setState({ folderTree: FileTreeUtils.createFolderTree(this.rootFolder) });
+			setFolderTree(FileTreeUtils.createFolderTree(rootFolder));
 		}
 		// After Each Vault Change Folder Count Map to Be Updated
-		if (this.plugin.settings.folderCount) this.setState({ folderFileCountMap: FileTreeUtils.getFolderNoteCountMap(this.plugin) });
-	};
-
-	render() {
-		return (
-			<React.Fragment>
-				{this.state.view === 'folder' ? (
-					<FolderComponent
-						plugin={this.plugin}
-						folderTree={this.state.folderTree}
-						activeFolderPath={this.state.activeFolderPath}
-						setActiveFolderPath={this.setActiveFolderPath}
-						setView={this.setView}
-						openFolders={this.state.openFolders}
-						setOpenFolders={this.setOpenFolders}
-						excludedFolders={this.state.excludedFolders}
-						setExcludedFolders={this.setExcludedFolders}
-						folderFileCountMap={this.state.folderFileCountMap}
-					/>
-				) : (
-					<FileComponent
-						plugin={this.plugin}
-						fileList={this.state.fileList}
-						setFileList={this.setFileList}
-						getFilesUnderPath={FileTreeUtils.getFilesUnderPath}
-						activeFolderPath={this.state.activeFolderPath}
-						setView={this.setView}
-						pinnedFiles={this.state.pinnedFiles}
-						setPinnedFiles={this.setPinnedFiles}
-						excludedExtensions={this.state.excludedExtensions}
-					/>
-				)}
-			</React.Fragment>
-		);
+		if (plugin.settings.folderCount) setFolderFileCountMap(FileTreeUtils.getFolderNoteCountMap(plugin));
 	}
+
+	return (
+		<React.Fragment>
+			{view === 'folder' ? (
+				<FolderComponent
+					plugin={plugin}
+					folderTree={folderTree}
+					activeFolderPath={activeFolderPath}
+					setActiveFolderPath={setActiveFolderPath}
+					setView={setView}
+					openFolders={openFolders}
+					setOpenFolders={setOpenFolders}
+					excludedFolders={excludedFolders}
+					setExcludedFolders={setExcludedFolders}
+					folderFileCountMap={folderFileCountMap}
+				/>
+			) : (
+				<FileComponent
+					plugin={plugin}
+					fileList={fileList}
+					setFileList={setFileList}
+					getFilesUnderPath={FileTreeUtils.getFilesUnderPath}
+					activeFolderPath={activeFolderPath}
+					setView={setView}
+					pinnedFiles={pinnedFiles}
+					setPinnedFiles={setPinnedFiles}
+					excludedExtensions={excludedExtensions}
+				/>
+			)}
+		</React.Fragment>
+	);
 }
