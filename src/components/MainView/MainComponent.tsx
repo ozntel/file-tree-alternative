9db1fe61772e362cdd3e21f1f519daa92c00a5ed
Ozan Tellioglu <ozntel@gmail.com>
2021-09-13 21:24:25 +0200

    Single View Separated from Main Component

--- src/components/MainView/MainComponent.tsx
+++ src/components/MainView/MainComponent.tsx
@@ -1,7 +1,8 @@
 import { TAbstractFile, TFile, TFolder } from 'obsidian';
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { FileComponent } from 'components/FileView/FileComponent';
 import { MainFolder } from 'components/FolderView/MainFolder';
+import { SingleView } from 'components/MainView/SingleView';
 import { FileTreeView } from 'FileTreeView';
 import FileTreeAlternativePlugin from 'main';
 import * as FileTreeUtils from 'utils/Utils';
@@ -162,54 +163,3 @@ export default function MainTreeComponent(props: MainTreeComponentProps) {
 		</React.Fragment>
 	);
 }
-
-const SingleView = (props: { plugin: FileTreeAlternativePlugin }) => {
-	const [dividerOnMove, setDividerOnMove] = useState<boolean>(false);
-	const [folderPaneHeight, setFolderPaneHeight] = useState<number>(null);
-	const [clientY, setClientY] = useState<number>(null);
-
-	let folderPaneRef = useRef<HTMLDivElement>();
-	let dividerRef = useRef<HTMLDivElement>();
-
-	function touchMouseStart(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-		e.preventDefault();
-		setDividerOnMove(true);
-		let height = dividerRef.current.offsetTop - folderPaneRef.current.offsetTop;
-		setFolderPaneHeight(height);
-		setClientY(e.nativeEvent.clientY);
-	}
-
-	function touchMouseMove(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-		e.preventDefault();
-		if (!dividerOnMove) return;
-		setFolderPaneHeight(folderPaneHeight + (e.nativeEvent.clientY - clientY));
-		setClientY(e.nativeEvent.clientY);
-	}
-
-	function touchMouseEnd(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-		e.preventDefault();
-		setDividerOnMove(false);
-		setClientY(e.nativeEvent.clientY);
-	}
-
-	return (
-		// Register Move & End Events for All File Tree Leaf
-		<div className="file-tree-container" onMouseMove={(e) => touchMouseMove(e)} onMouseUp={(e) => touchMouseEnd(e)}>
-			<div className="oz-folder-pane" ref={folderPaneRef} style={{ height: folderPaneHeight ? `${folderPaneHeight}px` : '50%' }}>
-				<MainFolder plugin={props.plugin} />
-			</div>
-
-			{/* Mouse Down Event only For Divider */}
-			<div
-				id="file-tree-divider"
-				ref={dividerRef}
-				onClick={(e) => e.preventDefault()}
-				onMouseDown={(e) => touchMouseStart(e)}
-				style={{ backgroundColor: dividerOnMove ? 'var(--interactive-accent)' : '' }}></div>
-
-			<div className="oz-file-list-pane">
-				<FileComponent plugin={props.plugin} />
-			</div>
-		</div>
-	);
-};
